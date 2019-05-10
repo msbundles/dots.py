ef21c764f8faa219a73f3b03f12e866d0a23d4cb
msbundles <25093511+msbundles@users.noreply.github.com>
2019-05-09 21:55:04 -0500

    Update dots.py

--- dots.py
+++ dots.py
@@ -10,51 +10,59 @@ browser.get('https://msbundles.github.io/Bundles-Personal-Programming-Betterment
 canvas = browser.find_element_by_tag_name('body')
 type(canvas)
 keyl = [Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT]
+
+
 #Base functions
 def up (iteration):
   for iteration in range(1,iteration):
     time.sleep(0.1)
     canvas.send_keys(Keys.UP)
     time.sleep(0.1)
+
 def down(iteration):
   for iteration in range(1, iteration):
     time.sleep(0.1)
     canvas.send_keys(Keys.DOWN)
     time.sleep(0.1)
+
 def left(iteration):
   for iteration in range(1, iteration):
     time.sleep(0.1)
     canvas.send_keys(Keys.LEFT)
     time.sleep(0.1)
+
 def right(iteration):
   for iteration in range(1, iteration):
     time.sleep(0.1)
     canvas.send_keys(Keys.RIGHT)
     time.sleep(0.1)
+
 def dist(iteration,dir):
   for iteration in range(1, iteration):
     time.sleep(0.1)
     canvas.send_keys(dir)
     time.sleep(0.1)
+
 def randcolor():
   for x in range(random.randrange(1, 4)):
     time.sleep(0.1)
     canvas.send_keys(2)
     time.sleep(0.1)
+
 def q(timee):
   time.sleep(timee)
   browser.quit()
 #end of base functions
 
 #main action functions
-def randdir(iteration1, iteration2):
+def randdir(iteration,):
   canvas.click()
-  for iteration1 in range(1, iteration1):
-    for iteration2 in range(1, iteration2):
-      randnum = random.randint(0,3)
-      time.sleep(0.1)
-      canvas.send_keys(keyl[randnum])
-      time.sleep(0.1)
+  for iteration in range(1, iteration):
+    random.shuffle(keyl)
+    randnum = random.randint(0,3)
+    time.sleep(0.1)
+    canvas.send_keys(keyl[randnum])
+    time.sleep(0.1)
 def mcs(u,d,l,r,w):
   canvas.click()
   dist(w, "w")
@@ -67,7 +75,18 @@ def mcs(u,d,l,r,w):
   randcolor()
   left(l)
 #end of main action functions
-if sys.argv[2] == "mcs":
-  mcs(sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
-elif sys.argv[2] == "rand":
-  randdir(sys.argv[3], sys.argv[4])
+if sys.argv[1] == "mcs":
+  in2 = int(sys.argv[2])
+  in3 = int(sys.argv[3])
+  in4 = int(sys.argv[4])
+  in5 = int(sys.argv[5])
+  in6 = int(sys.argv[6])
+  mcs(in2, in3, in4, in5, in6)
+elif sys.argv[1] == "rand":
+  in2 = int(sys.argv[2])
+  randdir(in2)
+else:
+  print("Pass arguments")
+  browser.quit()
+  time.sleep(2)
+  exit()
